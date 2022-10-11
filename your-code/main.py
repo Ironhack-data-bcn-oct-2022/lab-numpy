de9d9d1e1d4cfaffa958d859c46b89ed905e9da0
Bernat Bellmunt <bbellmunt@gmail.com>
2022-10-11 17:08:13 +0200

    Bernat commited exercises + bonus

--- your-code/main.py
+++ your-code/main.py
@@ -1,67 +1,87 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
+print(np.version.version)
 
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
+a = np.random.random((2,3,5))
 
 
 
 #4. Print a.
-
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b = np.ones((5,2,3))
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+if a.size == b.size:
+        print("the matrix a and b have the same size")
+elif a.size != b.size:
+        print("the matrix a and b do not have the same size")
 
 
 #8. Are you able to add a and b? Why or why not?
+if a.shape == b.shape:      
+        print("Matrix a and Matrix b can be added")
+elif a.shape != b.shape:
+        print("Matrix a and Matrix b cannot be added, as they have different shape")
 
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
+c = b.transpose(1,2,0)
+print(c)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
-
+d= a + c
+print(d)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
+print(a)
+print(d)
 
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e= a * c
+print(e)
 
 #13. Does e equal to a? Why or why not?
 
-
+e==a
+print("we are multiplying by a ones matrix")
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max= np.max(d)
+d_min= np.min(d)
+d_mean=np.mean(d)
 
+print(f"max is: {d_max}, min is: {d_min}, mean is: {d_mean}")
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
+f= np.empty((2,3,5))
+f.shape
+print (f)
 
 
 
@@ -75,6 +95,26 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
+for block in range(d.shape[0]):
+        for line in range (d.shape[1]):
+                for elem in range (d.shape[2]):
+                        if d[block][line][elem] > d_min and d[block][line][elem]< d_mean:
+                                f[block][line][elem] = 25
+                        elif d[block][line][elem]>d_mean and d[block][line][elem]<d_max:
+                                f[block][line][elem] = 75
+                        elif d[block][line][elem] == d_mean:
+                                f[block][line][elem] = 50
+                        elif d[block][line][elem] == d_min:
+                                f[block][line][elem] = 0
+                        elif d[block][line][elem] == d_max:
+                                f[block][line][elem]=100
+
+
+
+#f = np.where((d>d_min) & (d<d_mean),25,d)
+#f= np.where((d>d_mean) & (d<d_max),75,d)
+#f= np.where((d==d_mean),50,d)
+print(f)
 
 
 
@@ -98,7 +138,9 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+print(d)
+print(f"max is: {d_max}, min is: {d_min}, mean is: {d_mean}")
+print(f)
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -112,3 +154,23 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+
+g= np.empty((2,3,5),dtype="object")
+
+
+for block in range(d.shape[0]):
+        for line in range (d.shape[1]):
+                for elem in range (d.shape[2]):
+                        if d[block][line][elem] > d_min and d[block][line][elem]< d_mean:
+                                g[block][line][elem] = "B"
+                        elif d[block][line][elem]>d_mean and d[block][line][elem]<d_max:
+                                g[block][line][elem] = "D"
+                        elif d[block][line][elem] == d_mean:
+                                g[block][line][elem] = "C"
+                        elif d[block][line][elem] == d_min:
+                                g[block][line][elem] = "A"
+                        elif d[block][line][elem] == d_max:
+                                g[block][line][elem]="E"
+
+
+print (g)
