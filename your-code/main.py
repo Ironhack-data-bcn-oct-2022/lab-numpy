e55345a2e5b8b43d38bd64e2f1c7165e854087d8
joanrocajovani <joanrocajovani@gmail.com>
2022-10-11 17:20:56 +0200

    Uploading Lab

--- your-code/main.py
+++ your-code/main.py
@@ -1,68 +1,82 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
+print(np.__version__)
 
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
+a = np.random.randint(10, size = (2, 3, 5))
 
 
 #4. Print a.
-
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b = np.ones((5,2,3))
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
+if a.size == b.size:
+        print("Yes")
 
 
 
 #8. Are you able to add a and b? Why or why not?
-
+#sum = np.add(a,b)
+#Can't add because they have different shapes (2,3,5) (5,2,3)
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+c = b.transpose(1,2,0)
 
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
+d = np.add(a,c)
+print(d)
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
+print(a)
+print(d)
 
 
 
 #12. Multiply a and c. Assign the result to e.
-
-
+e = np.multiply(a,c)
+print(e)
 
 #13. Does e equal to a? Why or why not?
-
-
+if a.shape == e.shape:
+        print("Yes")
+else:
+        print("No")
+#Yes, because the shapes and the elements are the same
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
-
+d_min = np.amin(d)
+d_max = np.max(d)
+d_mean = np.mean(d)
+print(d_min)
+print(d_max)
+print(d_mean)
 
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
-
+f = np.empty((2,3,5))
+print(f)
 
 
 """
@@ -74,6 +88,24 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
+aux1 = len(f)
+aux2 = len(f[0])
+aux3 = len(f[0][0])
+
+for i in range(aux1):
+        for j in range(aux2):
+                for k in range(aux3):
+                        if d_mean < d[i][j][k] < d_max:
+                                f[i][j][k]=75
+                        elif d_min < d[i][j][k] < d_mean:
+                                f[i][j][k]=25
+                        elif d[i][j][k] == d_mean:
+                                f[i][j][k]=50
+                        elif d[i][j][k] == d_min:
+                                f[i][j][k]=0
+                        elif d[i][j][k] == d_max:
+                                f[i][j][k]=100
+print(f)
 
 
 
@@ -98,7 +130,8 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+print("d", d)
+print("f", f)
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -112,3 +145,18 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+k = np.empty((2,3,5),dtype=float)
+k = k.tolist()
+for i in range(len(d)):
+        for y in range(len(d[i])):
+                for z in range(len(d[i][y])):
+                        if d[i][y][z] > d_min and d[i][y][z] < d_mean:
+                                k[i][y][z] = "B"
+                        elif d[i][y][z] > d_mean and d[i][y][z] < d_max:
+                                k[i][y][z] = "C"
+                        elif d[i][y][z] == d_min:
+                                k[i][y][z] = "A"
+                        elif d[i][y][z] == d_max:
+                                k[i][y][z] = "D"
+k = np.array(k)
+print(k)
