6649e3f5a172ae28abaec9e9e9ad2604af0bd96e
Selma <slimaa.l3@gmail.com>
2022-10-11 18:14:03 +0200

    NumpyLab

--- your-code/main.py
+++ your-code/main.py
@@ -1,69 +1,78 @@
-#1. Import the NUMPY package under the name np.
+# 1. Import the NUMPY package under the name np.
+import numpy as np
 
+# 2. Print the NUMPY version and the configuration.
+print(np.__version__)
 
-
-#2. Print the NUMPY version and the configuration.
-
-
-
-#3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
+# 3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.randint(5, size=(2, 3, 5))
+
+# 4. Print a.
+print(a)
+
+# 5. Create a 5x2x3 3-dimensional array with all values equaling 1.
+# Assign the array to variable "b"
+b = np.ones((5, 2, 3))
 
 
-#4. Print a.
+# 6. Print b.
+print(b)
 
 
+# 7. Do a and b have the same size? How do you prove that in Python code?
 
-#5. Create a 5x2x3 3-dimensional array with all values equaling 1.
-#Assign the array to variable "b"
-
-
-
-#6. Print b.
+if a.size == b.size:
+    print(True)
 
 
+# 8. Are you able to add a and b? Why or why not?
+# print(a+b)
+# can't add a and b, because the shapes are not matching
+if a.shape != b.shape:
+    print(True)  # this is True
 
-#7. Do a and b have the same size? How do you prove that in Python code?
+# 9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+# a=2x3x5
+# b=5x2x3
+c = b.transpose(1, 2, 0)
+print(c)
 
+# 10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
+d = a+c
+print(d)
 
-
-#8. Are you able to add a and b? Why or why not?
-
-
-
-#9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
-
-
-#10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
-
-
-#11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
+# now it works because a and b have matching shapes
 
 
+# 11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
+print(a)
+print(d)
+# a is an a 3-dimensional array of integers, b 3-dimensional array of floats
 
-#12. Multiply a and c. Assign the result to e.
-
-
-
-#13. Does e equal to a? Why or why not?
-
-
-
-
-#14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
+# 12. Multiply a and c. Assign the result to e.
+e = a*c
+print(e)
 
 
+# 13. Does e equal to a? Why or why not?
+if e.size == a.size:
+    print(True)
 
+# 14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
+print(d_max)
+print(d_mean)
+print(d_min)
 
-#15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
+# 15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
-
+f = np.empty((2, 3, 5))
+print(f)
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -75,7 +84,24 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
+for index, block in enumerate(d):
+    for index1, row in enumerate(block):
+        for index2, i in enumerate(row):
 
+            if i > d_min and i < d_min:
+                f[index][index1][index2] = 25
+
+            if i > d_mean and i < d_max:
+                f[index][index1][index2] = 75
+
+            if i == d_mean:
+                f[index][index1][index2] = 50
+
+            if i == d_min:
+                f[index][index1][index2] = 0
+
+            if i == d_max:
+                f[index][index1][index2] = 100
 
 
 """
@@ -98,7 +124,8 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+print(f)
+print(d)
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
